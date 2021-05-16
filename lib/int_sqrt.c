48b66d11deaba6fae3b28360724ada8212d9936b
FlyFrog <ramgear@gmail.com>
2021-05-16 22:59:57 +0530

    lib: int_sqrt: improve 3x faster integer sqrt
    Result on 10,000,000 call.
    Old:
    sqrt(12345689) = 3513
    real	0m0.768s
    user	0m0.760s
    sys	0m0.004s
    
    New:
    sqrt(12345689) = 3513
    real	0m0.222s
    user	0m0.224s
    sys	0m0.000s
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Azuki <id_azuki@protonmail.com>

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -14,9 +14,11 @@
  *
  * A very rough approximation to the sqrt() function.
  */
-unsigned long int_sqrt(unsigned long x)
+inline unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+	register unsigned long tmp;
+	register unsigned long place;
+	register unsigned long root = 0;
 
 	if (x <= 1)
 		return x;
@@ -26,13 +28,14 @@ unsigned long int_sqrt(unsigned long x)
 		b = y + m;
 		y >>= 1;
 
-		if (x >= b) {
-			x -= b;
-			y += m;
+		if (x >= tmp)
+		{
+			x -= tmp;
+			root += place;
 		}
-		m >>= 2;
-	}
+		place >>= 2;
+	}while (place != 0);
 
-	return y;
+	return root;
 }
 EXPORT_SYMBOL(int_sqrt);
