81df8461b99332ae8bd51b1c9c047c270db6a9d3
KAGA-KOKO <kotakandis185@gmail.com>
2024-08-22 12:57:19 +0700

    Update ets_navi_input.c

--- drivers/input/oppo_fp_drivers/et520/ets_navi_input.c
+++ drivers/input/oppo_fp_drivers/et520/ets_navi_input.c
@@ -562,7 +562,7 @@ void navi_operator(struct work_struct *work)
     struct navi_struct *command = container_of(work, struct navi_struct, workq);
 
 #if TRANSLATED_COMMAND
-	et520_translated_command_converter(command->cmd, command->egistec);
+	translated_command_converter(command->cmd, command->egistec);
 #else
 	straight_command_converter(command->cmd, command->egistec);
 
@@ -659,7 +659,7 @@ static int nav_input_thread(void *et_spi)
 			nav_input_sig || kthread_should_stop());
 		mutex_lock(&driver_mode_lock);
 		list_for_each_entry_safe(acmd, tempcmd, &cmd_list.list, list) {
-			et520_translated_command_converter(acmd->cmd, egistec);
+			translated_command_converter(acmd->cmd, egistec);
 			list_del(&acmd->list);
 			kfree(acmd);
 		}
